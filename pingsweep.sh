1fb38fa613a769ada71b4abe2c62d6dd835a08be
Ado Maksic <adnanmaksic@u.boisestate.edu>
2026-02-02 11:13:24 -0700

    Update

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+if [[ $1 == "-h" ]]; then
+	echo = "Help"
+elif [[ $1 == "-p" ]]; then
+	echo = "sweeping"
+else
+	echo "usage: hi"
+fi
+
 base="onyxnode"
 
 for i in {1..200}
 do
 	echo = "$base$i"
-	ping -c 1 $curr >> ping.log
+	ping -c 1 $base$i >> ping.log
 done
